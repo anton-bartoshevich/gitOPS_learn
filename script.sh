129966f8d6394fac23e1b3cd67ffe4dac9c29a9e
Anton Bartoshevich <anton-bartas@mail.ru>
2023-02-26 09:11:04 +0000

    Pull request

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file1=/home/server/gitOPS_learn/file1.txt
-file2=/home/server/gitOPS_learn/file2.txt
-
-for file in $file1 $file2 
-do
-	echo "Count tabs in $file is `grep -P '\t' $file | wc -l`"
-done
